51a6d3dadc5b06f433cd4bbb1ad58d4d87c50f70
AnneLaure57 <>
2021-02-25 20:47:04 +0100

    correction : on JSON

--- src/main/java/fr/miage/sid/agentinternaute/agent/AgentInternaute.java
+++ src/main/java/fr/miage/sid/agentinternaute/agent/AgentInternaute.java
@@ -13,6 +13,7 @@ import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
+import net.minidev.json.JSONObject;
 
 public class AgentInternaute extends Agent {
 	
