3145249e525066f2b7437538ce6fe1ce314d61cd
AnneLaure57 <>
2021-02-26 21:24:07 +0100

    fix : change empl TicketBehaviour

--- src/main/java/fr/miage/sid/agentinternaute/api/SearchController.java
+++ src/main/java/fr/miage/sid/agentinternaute/api/SearchController.java
@@ -1,12 +1,25 @@
 package fr.miage.sid.agentinternaute.api;
 
+import java.net.URI;
+import java.util.Optional;
 import java.util.logging.Logger;
 
+import javax.transaction.Transactional;
+
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import fr.miage.sid.agentinternaute.agent.JadeAgentContainer;
+import fr.miage.sid.agentinternaute.entity.Profile;
+import fr.miage.sid.agentinternaute.service.ProfileService;
 import fr.miage.sid.agentinternaute.service.SearchService;
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +33,29 @@ public class SearchController {
 
 	private final SearchService service;
 	
+	private final ProfileService profilService;
+	
 	//TODO here send profile + type + title
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<?> searchArt(@RequestBody String title, Boolean type, Profile profile) {
+		Optional<Profile> profileOptional = profilService.getProfileByName(profile.getName());
+		return null;
+
+		/*if (profileOptional.isPresent())
+			return ResponseEntity.status(209).body("Profile already exists");
+		else {
+			Profile savedProfile = service.createOrUpdateProfile(profile);
+			
+			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(savedProfile.getId()).toUri();
+			
+			JadeAgentContainer.getInstance().createNewAgentInternaute(savedProfile.getName());
+			
+			return ResponseEntity.status(201).location(location).body(savedProfile);
+		}*/
+	}
+
 
 }
