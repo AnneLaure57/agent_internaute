dd5927c3ec483320a09f9cfd61196ce4cb590450
MassicardLouis <louis.massicard@orange.fr>
2021-02-28 10:37:10 +0100

    AgentDistributeur : fix bugged code.

--- src/main/java/fr/miage/sid/agentinternaute/mock/AgentDistributeur.java
+++ src/main/java/fr/miage/sid/agentinternaute/mock/AgentDistributeur.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import fr.miage.sid.agentinternaute.commons.PassingTime;
 import fr.miage.sid.agentinternaute.service.ProfileService;
 import jade.core.AID;
 import jade.core.Agent;
@@ -62,8 +63,8 @@ public class AgentDistributeur extends Agent {
 			protected void onTick() {
 				/********** WITHOUT BEHAVIOUR *****/
 				long tStart = System.currentTimeMillis();
-				System.out.println("Coucou, je suis up");
-				checkDate(tStart);
+				System.out.println("Hello, je suis up");
+				PassingTime.checkDate(tStart);
 				
 			}
 		} );
@@ -166,58 +167,7 @@ public class AgentDistributeur extends Agent {
 			ex.printStackTrace();
 		}
 	}
-	/*
-	 * Vérifier la date 
-	 */
-// 		1 min -> 1 j
-////	30 min -> 1 mois
-////	1h30 -> 3 mois
-////	3h -> 6 mois
-////	6h -> 12 mois
-////	24h -> 4 ans
-	// TODO : fix warnings
-	@SuppressWarnings("unused")
-	private void checkDate(long tStart) {
-		
-		//Set date format
-		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy'T'HH:mm:ss");
-		//Convert Date + Timer
-		Date currentDate = new Date();
-		Date date2 =new Date(tStart);
-		
-		String currentDateTimeString = dateFormat.format(currentDate);
-		String currentDateTimer = dateFormat.format(date2);
-		
-		System.out.println(" Date courante : " + currentDateTimeString +", timer : " + currentDateTimer);	
-		
-		//check diff between dates
-		int diff = currentDate.compareTo(date2);
-		Long newDiff = null;
-		
-//		 24h -> 4 ans
-		if (diff == TimeUnit.HOURS.toMillis(24)) {
-			newDiff = TimeUnit.DAYS.toMillis(4 * 365);
-//		6h -> 12 mois
-		} else if (diff == TimeUnit.HOURS.toMillis(6)) {
-			newDiff = TimeUnit.DAYS.toMillis(12 * 30);
-//		3h -> 6 mois
-		} else if (diff == TimeUnit.HOURS.toMillis(3)) {
-			newDiff = TimeUnit.DAYS.toMillis(6 * 30);
-//		 1h30 -> 3 mois
-		} else if (diff == TimeUnit.MINUTES.toMillis(90)) {
-			newDiff = TimeUnit.DAYS.toMillis(3 * 30);
-		
-//		30 min -> 1 mois
-		} else if (diff == TimeUnit.MINUTES.toMillis(30)) {
-			newDiff = TimeUnit.DAYS.toMillis(3 * 30);
-		
-//	 		1 min -> 1 j		
-		} else if (diff == TimeUnit.MINUTES.toMillis(1)) {
-			newDiff = TimeUnit.DAYS.toMillis(1);
-		} else {
-			System.out.println(currentDate + " is equal to " + date2);
-		}
-	} 
+
 	
 	/*
 	/*
