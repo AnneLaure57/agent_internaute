6ce984434360f9dd4365d9c9315aca2d5bb2f7a0
MassicardLouis <louis.massicard@orange.fr>
2021-02-28 13:54:17 +0100

    ACLMessageTypes : list ACL Messages possibles.

---
+++ src/main/java/fr/miage/sid/agentinternaute/commons/ACLMessageTypes.java
@@ -0,0 +1,12 @@
+package fr.miage.sid.agentinternaute.commons;
+
+/**
+ * @author Louis MASSICARD (user name : louis)
+ * @version 
+ * @since %G% - %U% (%I%)
+ *
+ */
+public class ACLMessageTypes {
+	/* ========================================= Global ================================================ */ /*=========================================*/
+		public final static String REQUEST_SEARCH = "search", REQUEST_PURCHASE = "purchase";
+}
