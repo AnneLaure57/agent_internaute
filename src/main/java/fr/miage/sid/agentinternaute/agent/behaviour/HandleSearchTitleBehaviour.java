482f73a389536e35b10320caad24d1904c587282
MassicardLouis <louis.massicard@orange.fr>
2021-03-02 22:27:32 +0100

    Search Title : OK

---
+++ src/main/java/fr/miage/sid/agentinternaute/agent/behaviour/HandleSearchTitleBehaviour.java
@@ -0,0 +1,74 @@
+package fr.miage.sid.agentinternaute.agent.behaviour;
+
+import java.util.logging.Logger;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.lang.acl.ACLMessage;
+
+public class HandleSearchTitleBehaviour extends CyclicBehaviour {
+	/* ========================================= Global ================================================ */ /*=========================================*/
+	
+	private static final long serialVersionUID = 1456892866260756940L;
+	
+	private static final Logger LOGGER = Logger.getLogger(HandleSearchTitleBehaviour.class.getName());
+	
+	/* ========================================= Attributs ============================================= */ /*=========================================*/
+
+	private JSONArray response;
+	
+	private DFAgentDescription distributor;
+	
+	/* ========================================= Constructeurs ========================================= */ /*=========================================*/
+
+	public HandleSearchTitleBehaviour(JSONArray response, DFAgentDescription distributor) {
+		super();
+		this.response = response;
+		this.distributor = distributor;
+	}
+	
+	/* ========================================= Methodes ============================================== */ /*=========================================*/
+
+//	@Override
+//	public void action() {
+//		// On récupère l'ACL message
+//		ACLMessage message = myAgent.receive();
+//		if (message != null) {
+//			// On récupère l
+//			String content = message.getContent();
+//			JSONObject JSON = new JSONObject(content);
+//			
+//			// Logique métier
+//			if (message.getPerformative() == ACLMessage.INFORM) {
+//				JSON.put("distributeur")
+//				
+//				// Transmit to API
+//				event.notifyProcessed(content);
+//			}
+//		}
+//	}
+	
+	@Override
+	public void action() {
+		// On récupère l'ACL message
+		ACLMessage message = myAgent.receive();
+		if (message != null) {
+
+			// Logique métier : on veut un ACLMessage.INFORM et 
+			if (message.getPerformative() == ACLMessage.INFORM && message.getSender().equals(this.distributor.getName())) {
+				LOGGER.info("You receive a message from : " + message.getSender());
+
+				// On récupère le JSON
+				String content = message.getContent();
+				JSONObject JSON = new JSONObject(content);
+				
+				// On ajoute le nom du distributeur
+				JSON.put("distributeur", message.getSender());
+				this.response.put(JSON);
+			}
+		}
+	}
+}
---
+++ src/main/java/fr/miage/sid/agentinternaute/agent/behaviour/ParallelSearchTitleBehaviour.java
@@ -0,0 +1,59 @@
+package fr.miage.sid.agentinternaute.agent.behaviour;
+
+import java.util.logging.Logger;
+
+import org.json.JSONArray;
+
+import jade.core.behaviours.ParallelBehaviour;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.util.Event;
+
+/**
+ * @author Louis MASSICARD (user name : louis)
+ * @version 
+ * @since %G% - %U% (%I%)
+ *
+ * Ex de doc : https://djug.developpez.com/java/jade/behaviours/
+ */
+public class ParallelSearchTitleBehaviour extends ParallelBehaviour {
+	/* ========================================= Global ================================================ */ /*=========================================*/
+
+	private static final long serialVersionUID = 4023562697885420376L;
+	
+	private static final Logger LOGGER = Logger.getLogger(ParallelSearchTitleBehaviour.class.getName());
+
+	/* ========================================= Attributs ============================================= */ /*=========================================*/
+
+//	private Event event;
+//	
+//	private DFAgentDescription[] distributors;
+//	
+//	private JSONArray response;
+	
+	/* ========================================= Constructeurs ========================================= */ /*=========================================*/
+
+	public ParallelSearchTitleBehaviour(Event event, DFAgentDescription[] distributors) {
+		super(ParallelBehaviour.WHEN_ALL);
+//		this.event = event;
+//		this.distributors = distributors;
+//		this.response = new JSONArray();
+		
+		JSONArray response = new JSONArray();
+		
+		LOGGER.info("Call the ParallelSearchTitleBehaviour.");
+		
+		for (DFAgentDescription distributor : distributors) {
+			this.addSubBehaviour(new HandleSearchTitleBehaviour(response, distributor));
+		}
+		
+		// Transmit to API
+		event.notifyProcessed(response.toString());
+	}
+	
+	/* ========================================= Methodes ============================================== */ /*=========================================*/
+
+	/* ========================================= Accesseurs ============================================ */ /*=========================================*/
+
+	/* ========================================= Main ================================================== */ /*=========================================*/
+
+}
